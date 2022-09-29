33afcccaba714c75916751c26dc579e781c8783c
Md. Tanvirul Islam <md.tanvirulislam@outlook.com>
2022-09-29 23:42:58 +0600

    other langs

---
+++ dart-language/learning/d_22_10_21_c1.dart
@@ -0,0 +1,10 @@
+void main() {
+  //String example
+  String name = 'Saad';
+  //int exmple
+  int age = 19;
+  var fals_age = 17;
+  print(fals_age);
+  print(age);
+  print(name);
+}
---
+++ dart-language/learning/d_23_10_21_c1.dart
@@ -0,0 +1,7 @@
+void main() {
+  final String person_name;
+  person_name = 'Saad';
+  // person_name = 'Saman';
+  // person_name = 'Mahi';
+  print(person_name);
+}
