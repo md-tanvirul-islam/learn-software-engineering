77fad80912a261d5612ed0b8da94b53b059d1d90
Md. Tanvirul Islam <md.tanvirulislam@outlook.com>
2022-10-09 05:59:35 +0600

    type

--- typescript/01_types.ts
+++ typescript/01_types.ts
@@ -1,14 +1,31 @@
  /* PRIMITIVE TYPES */
- //string
+    //string
 let myName: string = 'Tanvir';
- //number
+    //number
 let year: number = 2022;
- //boolean
+    //boolean
 let isEnable: boolean;
 isEnable = true;
- //null
- //undefined
+    //null
+let iamNull: null;
+iamNull = 'hi! I accept only null. Hihi!';
+    //undefined
+let iamUndefined: undefined = undefined;
  //symbol are unique constant value
+
+/* TypeScript Special types for special reasons */
+    //any
+let anyData: any = 'It can be anything';
+anyData = 2000;
+anyData = false;
+    //unknown
+let unknownData: unknown = "It's total unknown. Oh! my Allah.";
+unknownData = 2302
+unknownData = true;
+    //never
+let neverInMe: never;
+neverInMe = "I'm never. I will not allow to assign any data. Eahoo..!!";
+
 
 /* OBJECT TYPES */
 // array
