9f6c82d510372eb47263927e63586fe59f310a2c
Md. Tanvirul Islam <md.tanvirulislam@outlook.com>
2023-02-18 16:38:08 +0600

    trying python after a long time.

---
+++ python/lesson1.py
@@ -0,0 +1,5 @@
+print("Hi Raisha, How are you?")
+status = input("")
+print(f"ok. I understand. You are {status}")
+location =  input("Where is your father now?")
+print(f"O, you father is in {location} now. Grate. 🤡🙂G")
