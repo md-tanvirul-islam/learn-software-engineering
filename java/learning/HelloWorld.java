7da3b8afa9ae2e5b4f4abadf54ac39367a570250
Md. Tanvirul Islam <md.tanvirulislam@outlook.com>
2023-02-21 23:24:12 +0600

    ignore source file

--- java/learning/HelloWorld.java
+++ java/learning/HelloWorld.java
@@ -1,7 +1,14 @@
-public class HelloWorld
-{
-    public static void main(String[] $args)
-    {
-        System.out.println("hello world");
+// public class HelloWorld
+// {
+//     public static void main(String[] $args)
+//     {
+//         System.out.println("hello world");
+//     }
+// }
+
+
+public class HelloWorld {
+    public static void main(String[] $args){
+        System.out.println("Hello Word");
     }
 }
