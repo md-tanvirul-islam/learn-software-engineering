bae189011fdb836a79567ab89f651d28933e16c3
Md. Tanvirul Islam <md.tanvirulislam@outlook.com>
2024-04-08 20:14:41 +0600

    node vs browser

--- javascript/project/node/hello.js
+++ javascript/project/node/hello.js
@@ -7,3 +7,14 @@ if (mission == 'learn') {
 }
 
 console.log(process.argv);
+
+console.log(
+    `
+    Browser Vs Node
+    1. window to global \n
+    2. document to process \n
+    3. history to module \n
+    4. location to __filename, __dirname \n
+    5. navigator to require() \n
+    `
+);
