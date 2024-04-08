eb0a20f73c55b8f99ffb1cc0e7ec006ba537ae98
Md. Tanvirul Islam <md.tanvirulislam@outlook.com>
2024-04-08 20:02:59 +0600

    node arguments to file

---
+++ javascript/project/node/hello.js
@@ -0,0 +1,9 @@
+const mission = process.argv[2];
+
+if (mission == 'learn') {
+    console.log('Time to write some Node Code!');
+} else {
+    console.log(`Is ${mission} really more fun?`);
+}
+
+console.log(process.argv);
