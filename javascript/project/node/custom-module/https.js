3df72e920a2e911e649cc5632523d8ad447874fe
Md. Tanvirul Islam <md.tanvirulislam@outlook.com>
2024-04-09 12:38:10 +0600

    response and request

---
+++ javascript/project/node/custom-module/https.js
@@ -0,0 +1,17 @@
+// const request = require('./request.js');
+// const response = require('./response');
+
+// function communicate() {
+//     request.send('What is the name of your country?');
+//     response.receive('Bangladesh')
+// }
+
+const { send } = require('./request.js');
+const { receive } = require('./response');
+
+function communicate() {
+    send('What is the name of your country?');
+    receive('Bangladesh')
+}
+
+communicate()
