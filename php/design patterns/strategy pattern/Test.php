525d5c5f7975ad66d43263c2037ed607419d54ca
Md. Tanvirul Islam <md.tanvirulislam@outlook.com>
2025-04-17 07:41:58 +0600

    php strategy pattern

---
+++ php/design patterns/strategy pattern/Test.php
@@ -0,0 +1,7 @@
+<?php
+require_once __DIR__ . '/vendor/autoload.php';
+
+use Tanvir\StrategyPattern\Duck; 
+
+$duck =  new Duck;
+$duck->fly();
---
+++ php/design patterns/strategy pattern/src/Duck.php
@@ -0,0 +1,17 @@
+<?php 
+
+namespace Tanvir\StrategyPattern;
+
+class Duck{
+    public function swim(){
+        echo "Yes, I can swim";
+    }
+
+    public function fly(){
+        echo "Yes, I can fly";
+    }
+
+    public function quack(){
+        echo "I make sound like QuackA";
+    }
+}
